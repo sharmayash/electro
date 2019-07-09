df2938b8ce67fd8f6d81fd1c28866894325f5646
yash <sharmayash44ys@gmail.com>
2019-07-09 19:57:49 +0530

    prod updated

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -26,54 +26,58 @@ function ProductItem(props) {
   const classes = useStyles();
   const { products } = props;
 
-  return products.map(product => (
-    <Grid
-      item
-      xs={12}
-      sm={6}
-      md={4}
-      lg={4}
-      xl={3}
-      key={product._id}
-      style={{ padding: "2rem" }}
-    >
-      <Grow in>
-        <Card className={classes.card}>
-          <CardActionArea>
-            <CardMedia
-              component={Link}
-              to={`/product/${product._id}`}
-              className={classes.media}
-              image={product.image}
-              title={product.company}
-            />
-            <CardContent>
-              <Grid container>
-                <Grid item xs={8}>
-                  <Typography variant="h6" noWrap>
-                    {product.name}
-                  </Typography>
-                  <Typography variant="caption">
-                    by <i> {product.company} </i>
-                  </Typography>
+  if (products === undefined || products === null) {
+    return <h1>Loading ...</h1>;
+  } else {
+    return products.map(product => (
+      <Grid
+        item
+        xs={12}
+        sm={6}
+        md={4}
+        lg={4}
+        xl={3}
+        key={product._id}
+        style={{ padding: "2rem" }}
+      >
+        <Grow in>
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                component={Link}
+                to={`/product/${product._id}`}
+                className={classes.media}
+                image={product.image}
+                title={product.company}
+              />
+              <CardContent>
+                <Grid container>
+                  <Grid item xs={8}>
+                    <Typography variant="h6" noWrap>
+                      {product.name}
+                    </Typography>
+                    <Typography variant="caption">
+                      by <i> {product.company} </i>
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={4}>
+                    <Button
+                      component="span"
+                      variant="outlined"
+                      size="small"
+                      color="primary"
+                    >
+                      ${product.price}
+                    </Button>
+                  </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                  <Button
-                    component="span"
-                    variant="outlined"
-                    size="small"
-                    color="primary"
-                  >
-                    ${product.price}
-                  </Button>
-                </Grid>
-              </Grid>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Grow>
-    </Grid>
-  ));
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grow>
+      </Grid>
+    ));
+  }
 }
 
 ProductItem.propTypes = {
